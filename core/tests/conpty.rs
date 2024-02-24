f7d7940984b8cbc810e85f52333393be69e176a5
muji <muji@tmpfs.org>
2024-02-24 11:08:57 +0800

    Try explicit set_echo() false.

--- core/tests/conpty.rs
+++ core/tests/conpty.rs
@@ -4,9 +4,9 @@ use std::io::prelude::*;
 #[cfg(windows)]
 fn conpty_echo() {
     let mut proc = conpty::spawn("echo Hello World").unwrap();
-    proc.set_echo(true).unwrap();
+    proc.set_echo(false).unwrap();
     let mut reader = proc.output().unwrap();
-    //let mut writer = proc.output().unwrap();
+    let mut writer = proc.input().unwrap();
 
     let mut buf = [0; 1028];
     let n = reader.read(&mut buf).unwrap();
@@ -14,5 +14,10 @@ fn conpty_echo() {
     assert!(String::from_utf8_lossy(&buf).contains("Hello World"));
     
     println!("READ FROM THE PROGRAM OUTPUT");
+
+    drop(writer);
+    drop(reader);
+
+    proc.exit(1).unwrap();
 
 }
