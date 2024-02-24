88ed98e49baf4146404a2fe7c238f72fbb2056c7
muji <muji@tmpfs.org>
2024-02-24 11:05:48 +0800

    Debug conpty test.

--- core/tests/conpty.rs
+++ core/tests/conpty.rs
@@ -6,9 +6,13 @@ fn conpty_echo() {
     let mut proc = conpty::spawn("echo Hello World").unwrap();
     proc.set_echo(true).unwrap();
     let mut reader = proc.output().unwrap();
+    //let mut writer = proc.output().unwrap();
 
     let mut buf = [0; 1028];
     let n = reader.read(&mut buf).unwrap();
 
     assert!(String::from_utf8_lossy(&buf).contains("Hello World"));
+    
+    println!("READ FROM THE PROGRAM OUTPUT");
+
 }
